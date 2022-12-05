539584b9752267a0c9abd10cb5546ee9cee81cdd
alexanderbira <mail@alexbr.dev>
2022-12-05 13:12:03 +0000

    day 5 improvements

--- day05/day05.js
+++ day05/day05.js
@@ -8,36 +8,46 @@ const input = await readFile(join(__dirname, "input.txt"), "utf8");
 const parts = input.split("\n\n");
 
 const startingStuff = parts[0].split("\n");
-const stack = {};
-let numStacks = 0
-for (let i of startingStuff[startingStuff.length-1].trim().split("   ")) {
-  numStacks ++;
-  stack[i] = [];
+const stacks = {};
+let numStacks = 0;
+for (let i of startingStuff[startingStuff.length - 1].trim().split("   ")) {
+  numStacks++;
+  stacks[i] = [];
 }
 startingStuff.pop();
 for (let row of startingStuff) {
-  for (let i = 0; i < numStacks; i ++) {
-    if (row[(i*4)+1] !== " ") {
-      stack[i+1].unshift( row[(i*4)+1] );
+  for (let i = 0; i < numStacks; i++) {
+    if (row[i * 4 + 1] !== " ") {
+      stacks[i + 1].unshift(row[i * 4 + 1]);
     }
   }
 }
 
-const stack2 = JSON.parse(JSON.stringify(stack));
+const stack2 = JSON.parse(JSON.stringify(stacks));
 
 for (let instruction of parts[1].split("\n")) {
-  const numbers = instruction.slice(5).split(" from ");
-  const twoNums = numbers[1].split(" to ");
-  numbers[1] = twoNums[0];
-  numbers[2] = twoNums[1];
+  const numbers = instruction.match(/\d+/g);
 
-  for (let i = 0; i < parseInt(numbers[0]); i ++) {
-    stack[numbers[2]].push(stack[numbers[1]].pop());
+  for (let i = 0; i < parseInt(numbers[0]); i++) {
+    stacks[numbers[2]].push(stacks[numbers[1]].pop());
   }
 
-  const slice = stack2[numbers[1]].splice(stack2[numbers[1]].length-numbers[0], numbers[0])
-  stack2[numbers[2]] = [...stack2[numbers[2]], ...slice];
+  stack2[numbers[2]] = [
+    ...stack2[numbers[2]],
+    ...stack2[numbers[1]].splice(
+      stack2[numbers[1]].length - numbers[0],
+      numbers[0]
+    ),
+  ];
 }
 
-console.log(Object.keys(stack).map(i => stack[i][stack[i].length-1]).reduce((a, b)=>a+b,""))
-console.log(Object.keys(stack2).map(i => stack2[i][stack2[i].length-1]).reduce((a, b)=>a+b,""))
+console.log(
+  Object.keys(stacks)
+    .map((i) => stacks[i][stacks[i].length - 1])
+    .reduce((a, b) => a + b, "")
+);
+console.log(
+  Object.keys(stack2)
+    .map((i) => stack2[i][stack2[i].length - 1])
+    .reduce((a, b) => a + b, "")
+);
