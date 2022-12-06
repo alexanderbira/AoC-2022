15bbf772ab8abe47c66d1c67532de14d52cb737d
alexanderbira <mail@alexbr.dev>
2022-12-06 11:57:27 +0000

    better day 6

--- day06/day06.js
+++ day06/day06.js
@@ -5,41 +5,18 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const input = await readFile(join(__dirname, "input.txt"), "utf8");
 
-const lastFour = [input[0], input[1], input[2], input[3]];
 let start = 4;
-for (let letter of input.slice(4)) {
-  if (new Set(lastFour).size === lastFour.length) {
-    break;
-  }
-  lastFour.shift();
-  lastFour.push(letter);
-  start++;
-}
+for (
+  let slice = input.slice(start - 4, start);
+  start < input.length && !(new Set(slice).size === slice.length);
+  start++, slice = input.slice(start - 4, start)
+);
 console.log(start);
 
-const lastFourteen = [
-  input[0],
-  input[1],
-  input[2],
-  input[3],
-  input[4],
-  input[5],
-  input[6],
-  input[7],
-  input[8],
-  input[9],
-  input[10],
-  input[11],
-  input[12],
-  input[13],
-];
 start = 14;
-for (let letter of input.slice(14)) {
-  if (new Set(lastFourteen).size === lastFourteen.length) {
-    break;
-  }
-  lastFourteen.shift();
-  lastFourteen.push(letter);
-  start++;
-}
+for (
+  let slice = input.slice(start - 14, start);
+  start < input.length && !(new Set(slice).size === slice.length);
+  start++, slice = input.slice(start - 14, start)
+);
 console.log(start);
