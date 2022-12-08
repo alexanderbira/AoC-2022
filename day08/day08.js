41ef2478330750d93de46e0a2006d5f1f15019fa
alexanderbira <mail@alexbr.dev>
2022-12-08 12:32:05 +0000

    improve day 8

--- day08/day08.js
+++ day08/day08.js
@@ -3,110 +3,71 @@ const input = await read(import.meta.url);
 
 const lines = input.split("\n").map((line) => line.split("").map(Number));
 
-function checkVisible(row, col) {
+function getInfo(row, col) {
   const val = lines[row][col];
 
   // check if all values above are smaller
   let above = true;
+  let numAbove = 0;
   for (let i = row - 1; i >= 0; i--) {
+    numAbove++;
     if (lines[i][col] >= val) {
       above = false;
       break;
     }
   }
-  if (above) return true;
 
   // check if all values below are smaller
   let below = true;
+  let numBelow = 0;
   for (let i = row + 1; i < lines.length; i++) {
+    numBelow++;
     if (lines[i][col] >= val) {
       below = false;
       break;
     }
   }
-  if (below) return true;
 
   // check if all values left are smaller
   let left = true;
+  let numLeft = 0;
   for (let i = col - 1; i >= 0; i--) {
+    numLeft++;
     if (lines[row][i] >= val) {
       left = false;
       break;
     }
   }
-  if (left) return true;
 
   // check if all values right are smaller
   let right = true;
+  let numRight = 0;
   for (let i = col + 1; i < lines[row].length; i++) {
+    numRight++;
     if (lines[row][i] >= val) {
       right = false;
       break;
     }
   }
-  if (right) return true;
 
-  return false;
-}
-
-function checkScenic(row, col) {
-  const val = lines[row][col];
-
-  //check how many items are above until we find a bigger one
-  let above = 0;
-  for (let i = row - 1; i >= 0; i--) {
-    if (lines[i][col] < val) {
-      above++;
-    } else {
-      above++;
-      break;
-    }
-  }
-
-  //check how many items are below until we find a bigger one
-  let below = 0;
-  for (let i = row + 1; i < lines.length; i++) {
-    if (lines[i][col] < val) {
-      below++;
-    } else {
-      below++;
-      break;
-    }
-  }
-
-  //check how many items are left until we find a bigger one
-  let left = 0;
-  for (let i = col - 1; i >= 0; i--) {
-    if (lines[row][i] < val) {
-      left++;
-    } else {
-      left++;
-      break;
-    }
-  }
-
-  //check how many items are right until we find a bigger one
-  let right = 0;
-  for (let i = col + 1; i < lines[row].length; i++) {
-    if (lines[row][i] < val) {
-      right++;
-    } else {
-      right++;
-      break;
-    }
-  }
-
-  return above * below * left * right;
-  
+  return [
+    above || below || left || right,
+    numAbove * numBelow * numLeft * numRight,
+  ];
 }
 
 let count = 0;
 let maxScenic = 0;
+
 for (let row = 0; row < lines.length; row++) {
   for (let col = 0; col < lines[row].length; col++) {
-    if (checkVisible(row, col)) count++;
-    const scenic = checkScenic(row, col);
-    if (scenic > maxScenic) maxScenic = scenic;
+    const [visibe, scenic] = getInfo(row, col);
+    if (visibe) {
+      count++;
+    }
+    if (scenic > maxScenic) {
+      maxScenic = scenic;
+    }
   }
 }
 
