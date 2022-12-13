3615774dd293ac98d8da46f844d9a5966180db08
Alexander B-R <61094679+alexanderbira@users.noreply.github.com>
2022-12-13 17:47:16 +0000

    optimisations

--- day06/day06.js
+++ day06/day06.js
@@ -1,18 +1,15 @@
 import read from "../readFile.js";
 const input = await read(import.meta.url);
 
-let start = 4;
-for (
-  let slice = input.slice(start - 4, start);
-  start < input.length && !(new Set(slice).size === slice.length);
-  start++, slice = input.slice(start - 4, start)
-);
-console.log(start);
+const slice = (window) => {
+  let start = window;
+  for (
+    let slice = input.slice(start - window, start);
+    start < input.length && !(new Set(slice).size === slice.length);
+    start++, slice = input.slice(start - window, start)
+  );
+  console.log(start);
+};
 
-start = 14;
-for (
-  let slice = input.slice(start - 14, start);
-  start < input.length && !(new Set(slice).size === slice.length);
-  start++, slice = input.slice(start - 14, start)
-);
-console.log(start);
+slice(4);
+slice(14);
